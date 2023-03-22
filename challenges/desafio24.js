7d621f5e64cbd07f3c9907f2eb5e060d3f490515
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-03-22 20:34:05 -0300

    feat: Completa query MongoDb do requisito 24, e finaliza 100% do projetogit add .

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,9 +1,13 @@
 db.produtos.updateMany(
     {},
     {
-        valoresNutricionais: {
-                $each: ["combo", "tasty"],
-                $sort: 1,
+        $push: {
+            valoresNutricionais: {
+                $each: [],
+                $sort: { percentual: -1 },
             },
+        },
     },
 );
+
+db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0 });
