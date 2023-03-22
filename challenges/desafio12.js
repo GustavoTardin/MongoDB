1c1ebe876b2949b2a4fbd3b946b60b43c93c6208
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-03-22 19:32:48 -0300

    feat: Completa query MongoDb do requisito 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,2 +1,5 @@
-db.produtos.find({ $nor: [{ nome: /Big Mac/ }, { nome: /McChicken/ }] },
-    { nome: 1, valoresNutricionais: 1, ingredientes: 1, _id: 0 });
+db.produtos.updateMany(
+    { nome: { $ne: "McChicken" } },
+    { $addToSet: { ingredientes: "ketchup" } },
+);
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
