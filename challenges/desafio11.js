3d1381a6b7b616a7b8a6ba2c426ffc78121f7764
Gustavo Tardin <gustavofilipetardin@outlook.com>
2023-03-22 16:10:14 -0300

    feat: Completa query MongoDb do requisito 11

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,2 +1,2 @@
-db.produtos.find({ $and: [{ nome: { $not: /Big Mac/ } }, { nome: { $not: /McChicken/ } }] },
+db.produtos.find({ $nor: [{ nome: /Big Mac/ }, { nome: /McChicken/ }] },
     { nome: 1, curtidas: 1, vendidos: 1, _id: 0 });
